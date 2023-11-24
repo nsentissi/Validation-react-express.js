3b46497b711f73507414cf1c5c5a198ad74bfb69
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-11-24 12:08:32 +0100

    connected the database and the routes on the client side

---
+++ client/src/components/TaskCreate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskCreate = () => {
+  return (
+    <div>
+      this is where the task form
+    </div>
+  )
+}
+
+export default TaskCreate
---
+++ client/src/components/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div>
+      This is the last list
+    </div>
+  )
+}
+
+export default TaskList
